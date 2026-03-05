da613cfad9b9c4f268728f5e7e4d7c789bc19774
LLuis Lucas <llucas@cendrassos.net>
2026-03-05 13:56:11 +0100

    metode mou

--- Tasca/Animal.cs
+++ Tasca/Animal.cs
@@ -1,24 +1,25 @@
 public abstract class Animal
 {
     private static Random r = new();
-    public (int x, int y) Posicio_inicial {get; private set;}
+    public (int x, int y) Posicio {get; private set;}
 
     public (int x, int y) Direccio {get; private set;}
 
     public bool EsViu {get; private set;}
 
-    public Animal()
+    public Animal((int x, int y) posicio)
     {
+      
+      Posicio = posicio;
       direccioAnimal();
       EsViu = true;
 
     }
-
     
-    public void direccioAnimal ()
+    public virtual void direccioAnimal ()
     {
         string[] direccions = {"dreta","esquerra","amunt","abaix"};
-        string index = direccions[r.Next(direccions.Length-1)];
+        string index = direccions[r.Next(direccions.Length)];
         
        
        
@@ -43,9 +44,13 @@ public abstract class Animal
         
     }
 
-    public void MoureAnimal( (int x, int y) inici,(int x, int y) direccio )
+    public virtual void MoureAnimal( (int x, int y) posicioActual,(int x, int y) direccio )
     {
         
+        posicioActual.x = posicioActual.x +direccio.x;
+        posicioActual.y = posicioActual.y +direccio.y;
+        posicioActual= Posicio;
+
     }
 
     // 
